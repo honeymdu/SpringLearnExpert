e7ab41802f5714182bd7c60cc0414fbe70583182
Himanshu Mehra <97497671+honeymdu@users.noreply.github.com>
2024-09-23 14:46:11 +0530

    Initial Commit

---
+++ src/main/java/com/example/SpringLearnExpert/SpringLearnExpertApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringLearnExpert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearnExpertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearnExpertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringLearnExpert/SpringLearnExpertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringLearnExpert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearnExpertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
