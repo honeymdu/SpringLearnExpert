0d95e6900be6c553b85b937adc9da8d4a7964f7d
Himanshu Mehra <97497671+honeymdu@users.noreply.github.com>
2024-09-24 15:59:55 +0530

    Basic Project for Spring

--- src/main/java/com/example/SpringLearnExpert/Entitys/EmployeeEntity.java
+++ src/main/java/com/example/SpringLearnExpert/Entitys/EmployeeEntity.java
@@ -16,7 +16,7 @@ import lombok.Data;
 public class EmployeeEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
